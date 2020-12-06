645794d1d97d7616a77f78211b5710904ae76ca9
Navya140311 <72936140+Navya140311@users.noreply.github.com>
2020-12-06 23:02:17 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,2 @@
+document.getElementById("player_question").innerHTML = "Question turn:" + localStorage.getItem("player1_name");
+document.getElementById("player_answer").innerHTML = "Answer turn:" + localStorage.getItem("player2_name");
